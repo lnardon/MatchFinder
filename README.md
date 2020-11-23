4bcad14a018ecf473860400598727bb24604b2ef
lnardon <lucaslnardon@gmail.com>
2020-11-23 00:40:00 -0300

    ✨ Add leaflet map

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Basic React Boilerplate Project
+# MatchFinder
+
+MatchFinder is a website to find teams and locations to play sports.
--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import "./app.css";
+import Homepage from "./pages/Homepage";
 
 const App = () => {
   return (
     <Router>
-        <Switch>
-          <Route path="/" exact>
-
-          </Route>
-        </Switch>
+      <Switch>
+        <Route path="/" exact>
+          <Homepage />
+        </Route>
+      </Switch>
     </Router>
   );
-}
+};
 
 export default App;
