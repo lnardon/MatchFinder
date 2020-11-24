c246433cdc81859687b7f9f9e3f06c984c5e8d47
lnardon <lucaslnardon@gmail.com>
2020-11-23 23:17:24 -0300

    ✨ Add browser geolocation

--- src/pages/Homepage/index.jsx
+++ src/pages/Homepage/index.jsx
@@ -1,26 +1,38 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 function Homepage() {
-  const position = [51.505, -0.09];
-  return (
+  const [latLong, setLatLong] = useState();
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setLatLong({
+          lat: position.coords.latitude,
+          long: position.coords.longitude,
+        });
+      });
+    }
+  }, []);
+
+  return latLong ? (
     <MapContainer
-      center={[51.505, -0.09]}
+      center={[latLong.lat, latLong.long]}
       zoom={13}
       scrollWheelZoom={true}
-      style={{ height: "100vh", width: "100vh" }}
+      style={{ height: "100vh", width: "100vw" }}
     >
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
+      {/* <Marker position={[latLong.lat, latLong.long]}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
-      </Marker>
+      </Marker> */}
     </MapContainer>
-  );
+  ) : null;
 }
 
 export default Homepage;
